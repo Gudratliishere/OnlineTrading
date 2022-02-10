799efabc17b7dbf6648227ed188a2e2252f18fe2
Dunay Gudratli <d.qudretli@gmail.com>
2022-02-10 15:09:31 +0300

    categorydao completed

--- OnlineTradingDBWithJDBC/src/main/java/OnlineTradingDB/configuration/Context.java
+++ OnlineTradingDBWithJDBC/src/main/java/OnlineTradingDB/configuration/Context.java
@@ -1,7 +1,9 @@
 package OnlineTradingDB.configuration;
 
+import OnlineTradingDB.dao.impl.CategoryDAOImpl;
 import OnlineTradingDB.dao.impl.CityDAOImpl;
 import OnlineTradingDB.dao.impl.UserDAOImpl;
+import OnlineTradingDB.dao.inter.CategoryDAO;
 import OnlineTradingDB.dao.inter.CityDAO;
 import OnlineTradingDB.dao.inter.UserDAO;
 
@@ -9,11 +11,21 @@ public class Context
 {
     private static CityDAO cityDAO = null;
     private static UserDAO userDAO = null;
+    private static CategoryDAO categoryDAO = null;
+
+    public static CategoryDAO getCategoryDAO ()
+    {
+        if (categoryDAO == null)
+            categoryDAO = new CategoryDAOImpl();
+
+        return categoryDAO;
+    }
 
     public static UserDAO getUserDAO ()
     {
         if (userDAO == null)
             userDAO = new UserDAOImpl();
+
         return userDAO;
     }
 
--- OnlineTradingDBWithJDBC/src/main/java/OnlineTradingDB/main/Main.java
+++ OnlineTradingDBWithJDBC/src/main/java/OnlineTradingDB/main/Main.java
@@ -1,22 +1,16 @@
 package OnlineTradingDB.main;
 
 import OnlineTradingDB.configuration.Context;
-import OnlineTradingDB.dao.inter.CityDAO;
-import OnlineTradingDB.dao.inter.UserDAO;
-import OnlineTradingDB.entity.City;
-import OnlineTradingDB.entity.User;
+import OnlineTradingDB.dao.inter.CategoryDAO;
+import OnlineTradingDB.entity.Category;
 
 public class Main
 {
     public static void main (String[] args)
     {
-        CityDAO cityDAO = Context.getCityDAO();
+        CategoryDAO categoryDAO = Context.getCategoryDAO();
 
-        City city = cityDAO.getById(4L);
-
-        UserDAO userDAO = Context.getUserDAO();
-
-        User user = userDAO.getById(4L);
-        userDAO.remove(user);
+        Category category = categoryDAO.getById(8L);
+        categoryDAO.remove(category);
     }
 }
